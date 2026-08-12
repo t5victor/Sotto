e56e3742e2ec40f1a07d8f76f2cd85d3f4c6eb0f
Víctor Moya <xvictormoya@gmail.com>
2026-08-12 12:30:00 +0200

    build: define Sotto Swift package targets

---
+++ Package.swift
@@ -0,0 +1,59 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Sotto",
+    platforms: [
+        .macOS(.v14),
+    ],
+    products: [
+        .executable(name: "Sotto", targets: ["Sotto"]),
+        .executable(name: "SottoDoctor", targets: ["SottoDoctor"]),
+        .library(name: "SottoDesignSystem", targets: ["SottoDesignSystem"]),
+        .library(name: "SottoCore", targets: ["SottoCore"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/FluidInference/FluidAudio.git",
+            exact: "0.15.5"
+        ),
+    ],
+    targets: [
+        .target(name: "SottoDesignSystem"),
+        .target(
+            name: "SottoCore",
+            dependencies: [
+                .product(name: "FluidAudio", package: "FluidAudio"),
+            ]
+        ),
+        .executableTarget(
+            name: "Sotto",
+            dependencies: [
+                "SottoCore",
+                "SottoDesignSystem",
+            ],
+            exclude: [
+                "Resources/AppIcon.iconset",
+                "Resources/Assets.xcassets",
+                "Resources/AppIcon.icns",
+                "Resources/Info.plist",
+                "Resources/Sotto.entitlements",
+                "Resources/ThirdPartyNotices.txt",
+            ],
+            resources: []
+        ),
+        .executableTarget(
+            name: "SottoDoctor",
+            dependencies: ["SottoCore"]
+        ),
+        .testTarget(
+            name: "SottoDesignSystemTests",
+            dependencies: ["SottoDesignSystem"]
+        ),
+        .testTarget(
+            name: "SottoCoreTests",
+            dependencies: ["SottoCore"]
+        ),
+    ]
+)
